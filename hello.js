030215073ef6ead97b1c4b9084206316ddab5831
rachmankamil <rachmankamil24@gmail.com>
2023-07-12 21:07:06 +0700

    add hello js

---
+++ hello.js
@@ -0,0 +1,33 @@
+console.log("Hello World!")
+console.log("Hasil dari 10+5 adalah", 10+5)
+
+var arrayCustom = ["salma", 20, true, "ryan", ["kamil", 22]]
+
+console.log(arrayCustom[0]+ " dan "+arrayCustom[3] + "  .. cieeeeee! ")
+
+var string1 = "Ridho"
+console.log("length of string "+ string1 + " is " + string1.length)
+console.log("huruf ke 2 dari "+string1+" adalah "+ string1[1])
+
+var a = "123"
+var b = 12
+var sum = a+b
+console.log(sum)
+
+console.log(123)
+
+var x = 10
+if (true){
+    var x = 20
+    console.log("inside if "+x)
+}
+console.log("outer if "+x)
+
+console.log("/n let /n");
+let d = 10
+if (true){
+    const d = 20
+    console.log("inside if "+d)
+}
+d=50
+console.log("outer if "+d)
